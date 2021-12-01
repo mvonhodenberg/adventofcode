e7e81c3649cd699812ada2b1fda1c28ffaef1229
mvonhodenberg <mjvonhodenberg@gmail.com>
2021-12-01 19:57:15 +0000

    first commit

---
+++ day1.py
@@ -0,0 +1,23 @@
+from typing import Counter
+
+
+f=open('depths.txt','r')
+depths=f.read().split('\n')
+depths=[int(x) for x in depths]
+def FindIncreases(l):
+    c=0
+    for i in range(len(l)-1):
+        if l[i+1]>l[i]:
+            c+=1
+    return c
+def part1():
+    print(FindIncreases(depths))
+def part2():
+    i=0
+    windows=[]
+    while i+2<len(depths):
+        windows.append(sum(depths[i:i+3]))
+        i+=1
+    print(FindIncreases(windows))
+part1()
+part2()
