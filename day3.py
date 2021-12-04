3927fb3d735e53459a2899441d41bd1e981bf6bd
mvonhodenberg <mjvonhodenberg@gmail.com>
2021-12-04 14:53:04 +0000

    day3

---
+++ day3.py
@@ -0,0 +1,45 @@
+from collections import Counter
+f=open('day3input.txt','r')
+nums=f.read().split('\n')
+def part1():
+    gamma=''
+    epsilon=''
+    for i in range (len(nums[0])):
+        data = Counter([a[i] for a in nums])
+        gamma+=data.most_common(1)[0][0]
+        epsilon+=data.most_common()[1][0]
+    print(int(gamma,2)*int(epsilon,2))
+
+def part2():
+    l=nums
+    i=0
+    while len(l)>1:
+        data = Counter([a[i] for a in l])
+        try:
+            if data.most_common()[0][1]==data.most_common()[1][1]:
+                digit='1'
+            else:
+                digit=data.most_common()[0][0]
+            l=[a for a in l if a[i]==digit]
+        except IndexError:
+            pass
+        i+=1
+    oxygen=int(l[0],2)
+    m=nums
+    j=0
+    while len(m)>1:
+        data = Counter([a[j] for a in m])
+        try:
+            if data.most_common()[0][1]==data.most_common()[1][1]:
+                digit='0'
+            else:
+                digit=data.most_common()[1][0]
+            m=[a for a in m if a[j]==digit]
+        except IndexError:
+            pass
+        j+=1
+    co2=int(m[0],2)
+    print(oxygen*co2)
+
+part1()
+part2()
