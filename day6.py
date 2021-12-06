09f88318aacb90fd630a168860a610db1ffdaae5
mvonhodenberg <mjvonhodenberg@gmail.com>
2021-12-06 21:09:04 +0000

    day 6 impovements

--- day6.py
+++ day6.py
@@ -1,13 +1,13 @@
 def numfish(days):
     f=open('day6input.txt','r').read().split(',')
-    fish=[[i,f.count(str(i))] for i in range(9)]
+    fish=[f.count(str(i)) for i in range(9)]
     for d in range(days):
-        a=fish[0][1]
-        fish[0][1]=0
+        a=fish[0]
+        fish[0]=0
         for i in range(0,8):
-            fish[i][1]=fish[i+1][1]
-        fish[6][1]+=a
-        fish[8][1]=a
-    return sum([f[1] for f in fish])
+            fish[i]=fish[i+1]
+        fish[6]+=a
+        fish[8]=a
+    return sum(fish)
 print('part 1:', numfish(80))
 print('part 2:', numfish(256))
